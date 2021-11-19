e2a268cbe5cbf5a69e9b0b02e1dc11022b38e2d6
Fallenpetal <93207655+Fallenpetal@users.noreply.github.com>
2021-11-19 10:55:45 +0800

    add IntList.java

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,12 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        IntList ptr=A;
+        while(ptr.rest!=null){
+            ptr=ptr.rest;
+        }
+        ptr.rest=B;
+        return A;
     }
 
     /**
@@ -90,9 +94,22 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return null;
+        }
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+        A=A.rest;
+        while (A != null) {
+            ptr.rest = new IntList(A.first, null);
+            ptr = ptr.rest;
+            A=A.rest;
+        }
+        ptr.rest = B;
+        return res;
     }
+
+
 
 
 
