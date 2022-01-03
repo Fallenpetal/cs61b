4e1f956736fa6c72d6097b8eec0dc71ec7cacc53
Fallenpetal <93207655+Fallenpetal@users.noreply.github.com>
2022-01-03 18:51:30 +0800

    一定要遵循hw的要求,pluck需要先全部删除再添加，由于我没有写删除，导致前6次测试都失败了

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -19,7 +19,7 @@ public class GuitarString {
         int capacity = (int) Math.round(SR / frequency);
         buffer = new ArrayRingBuffer<>(capacity);
         for (int i = 0; i < capacity; i++) {
-            buffer.enqueue(0);
+            buffer.enqueue(0.0);
         }
     }
 
